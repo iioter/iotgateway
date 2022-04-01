66a2954438f70ac1b44bab2fde6e4fadd74ffc9e
dd <535915157@qq.com>
2022-04-01 21:48:00 +0800

    解决设备未启动页面报错的问题

--- IoTGateway.ViewModel/BasicData/DeviceVariableVMs/DeviceVariableListVM.cs
+++ IoTGateway.ViewModel/BasicData/DeviceVariableVMs/DeviceVariableListVM.cs
@@ -45,10 +45,12 @@ namespace IoTGateway.ViewModel.BasicData.DeviceVariableVMs
                 foreach (var item in device.Children)
                 {
                     var deviceThread = deviceService.DeviceThreads.Where(x => x._device.ID.ToString() == (string)item.Value).FirstOrDefault();
+                    if (deviceThread != null)
+                        item.Icon = deviceThread._device.AutoStart ? (deviceThread._driver.IsConnected ? "layui-icon-link" : "layui-icon-unlink") : "layui-icon-pause";
 
                     item.Text = item.Text;
-                    item.Icon = deviceThread._device.AutoStart ? (deviceThread._driver.IsConnected ? "layui-icon-link" : "layui-icon-unlink") : "layui-icon-pause";
                     item.Expended = true;
+
                 }
             }
             DevicesTree = GetLayuiTree(AllDevices);
