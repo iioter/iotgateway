dc126a51356f6c552525df8422f07984c8d0c982
liushoushan <ccliushou@126.com>
2022-06-06 15:15:19 +0800

    通过Mqtt 方式将数据写入IotDB

---
+++ Plugins/PluginInterface/IotDB/IotTsData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PluginInterface.IotDB
+{
+    /// <summary>
+    /// IotDB时序数据库，测点数据json定义
+    /// </summary>
+    public class IotTsData
+    {
+        public string device { get; set; }
+        public List<string> measurements { get; set; }
+        public List<object> values { get; set; }
+        public long timestamp { get; set; }
+    }
+}
