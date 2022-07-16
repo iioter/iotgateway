d9f205a5411831b64a5406cf89b75daff67d32df
iioter <535915157@qq.com>
2022-07-16 23:35:16 +0800

    提供OPC DA客户端

---
+++ Plugins/Drivers/DriverOPCDaClient/OPCClient/OPCDataChangedHandler.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Automation.OPCClient
+{
+    public delegate void OPCDataChangedHandler(List<OPCChangeModel> arg);
+   
+}
