4f5d897ac16ca781c308ec68fc56b5355db46019
iioter <535915157@qq.com>
2022-08-23 13:47:00 +0800

    升级wtm基础框架至6.1.0

--- WalkingTec.Mvvm/WalkingTec.Mvvm.TagHelpers.LayUI/Models/LayuiTreeItem.cs
+++ WalkingTec.Mvvm/WalkingTec.Mvvm.TagHelpers.LayUI/Models/LayuiTreeItem.cs
@@ -7,17 +7,44 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
 {
     public class LayuiTreeItem
     {
-        [JsonPropertyName("title")]
+        [JsonPropertyName("name")]
         public string Title { get; set; }
+
+        [JsonPropertyName("value")]
+        public string Id { get; set; }
+
+        [JsonPropertyName("children")]
+        public List<LayuiTreeItem> Children { get; set; }
+
+        [JsonPropertyName("href")]
+        public string Url { get; set; }
+
+        [JsonPropertyName("spread")]
+        public bool Expand { get; set; }
+
+        [JsonPropertyName("selected")]
+        public bool Checked { get; set; }
+
+        [JsonPropertyName("disabled")]
+        public bool Disabled { get; set; }
+
+        [JsonPropertyName("level")]
+        public int Level { get; set; }
 
         [JsonPropertyName("icon")]
         public string Icon { get; set; }
+    }
+
+    public class LayuiTreeItem2
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
 
         [JsonPropertyName("id")]
         public string Id { get; set; }
 
         [JsonPropertyName("children")]
-        public List<LayuiTreeItem> Children { get; set; }
+        public List<LayuiTreeItem2> Children { get; set; }
 
         [JsonPropertyName("href")]
         public string Url { get; set; }
@@ -31,4 +58,5 @@ namespace WalkingTec.Mvvm.TagHelpers.LayUI
         [JsonPropertyName("level")]
         public int Level { get; set; }
     }
+
 }
