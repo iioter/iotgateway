5c78bb5b955caac88f4bad7240438453d1b38fd3
iioter <535915157@qq.com>
2023-04-16 10:18:10 +0800

    remove old angular frontend

--- IoTGateway/ClientApp/src/app/routes/dashboard/dashboard.component.ts
+++
@@ -1,8 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
-})
-export class DashboardComponent {}
