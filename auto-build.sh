ac3ac0c6d229383a3734c64cc5d91815738a2af3
iioter <535915157@qq.com>
2023-05-24 22:39:19 +0800

    优化dockerfile和脚本

--- auto-build.sh
+++ auto-build.sh
@@ -1,22 +1,8 @@
 #!/bin/bash
 echo start
-tag="2.0.0"
+tag="2.0.2"
 echo $tag
-docker build -t iotgateway .
-
-docker stop iotgateway
-docker rm iotgateway
-
-docker run -d --restart always --name iotgateway -p 518:518 -p 1888:1888 -p 62541:62541 -p 503:503 iotgateway
-
-docker cp 3d iotgateway:app/wwwroot/
-docker cp drivers iotgateway:app/
-docker cp IoTGateway/iotgateway.db iotgateway:app/
-docker cp IoTGateway/Quickstarts.ReferenceServer.Config.xml iotgateway:app/
-
-docker restart iotgateway
-
-docker commit iotgateway registry.cn-hangzhou.aliyuncs.com/iotgateway/iotgateway:$tag
+docker build -t registry.cn-hangzhou.aliyuncs.com/iotgateway/iotgateway:$tag .
 docker push registry.cn-hangzhou.aliyuncs.com/iotgateway/iotgateway:$tag
 
 docker tag registry.cn-hangzhou.aliyuncs.com/iotgateway/iotgateway:$tag registry.cn-hangzhou.aliyuncs.com/iotgateway/iotgateway:latest
