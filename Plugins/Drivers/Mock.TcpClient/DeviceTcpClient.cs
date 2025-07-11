815003480c38f382aebe946a9b811cbeaee797a0
LMW <42261363+FLC-GGBOOM@users.noreply.github.com>
2025-07-11 14:40:04 +0800

    Update DeviceTcpClient.cs

--- Plugins/Drivers/Mock.TcpClient/DeviceTcpClient.cs
+++ Plugins/Drivers/Mock.TcpClient/DeviceTcpClient.cs
@@ -20,6 +20,7 @@ namespace Mock.TcpClient
 
         public ILogger _logger { get; set; }
         private readonly string _device;
+        public event Func<object, DataReportEventArgs, Task>? OnDataReceived;
 
         #region 配置参数
 
